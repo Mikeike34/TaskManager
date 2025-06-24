26edd1a1131656660d44d892d8fd05ca441237c6
Mikeike34 <michaeljcantone@gmail.com>
2025-06-24 14:06:29 -0700

    Created CRUD API handlers

--- main.go
+++ main.go
@@ -9,14 +9,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Todo struct {
-	ID        int    `json:"_id" bson:"_id"`
-	Completed bool   `json:"completed" bson:"completed"`
-	Body      string `json:"body" bson:"body"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"` //omits empty values so we do not see IDs of all zeros
+	Completed bool               `json:"completed" bson:"completed"`
+	Body      string             `json:"body" bson:"body"`
 }
 
 var collection *mongo.Collection
@@ -49,10 +50,11 @@ func main() {
 
 	app := fiber.New()
 
+	//API Endpoints
 	app.Get("/api/todos", getTodos)
-	//app.Post("/api/todos", createTodo)
-	//app.Patch("/api/todos/:id", updateTodo)
-	//app.Delete("/api/todos/:id", deleteTodo)
+	app.Post("/api/todos", createTodo)
+	app.Patch("/api/todos/:id", updateTodo)
+	app.Delete("/api/todos/:id", deleteTodo)
 
 	port := os.Getenv("PORT") //port vartiable that holds the value of our PORT environment variable in .env
 	if port == "" {           //if the port is empty, use the hard value of 5000
@@ -61,6 +63,8 @@ func main() {
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
+
+//API handlers
 
 func getTodos(c *fiber.Ctx) error {
 	var todos []Todo //variable that holds an array of Todo structs
@@ -73,7 +77,7 @@ func getTodos(c *fiber.Ctx) error {
 
 	defer cursor.Close(context.Background())
 
-	for cursor.Next(context.Background()) {
+	for cursor.Next(context.Background()) { //uses the cursor to loop through all todos in database and append them to our todos array
 		var todo Todo
 
 		if err := cursor.Decode(&todo); err != nil {
@@ -82,9 +86,64 @@ func getTodos(c *fiber.Ctx) error {
 		todos = append(todos, todo)
 	}
 
-	return c.JSON(todos)
+	return c.JSON(todos) //returns the todos array as a JSON
 }
 
-//func createTodo(c *fiber.Ctx) error{}
-//func updateTodo(c *fiber.Ctx) error{}
-//func deleteTodo(c *fiber.Ctx) error{}
+func createTodo(c *fiber.Ctx) error {
+	todo := new(Todo)
+
+	if err := c.BodyParser(todo); err != nil { //binds a request body to a struct (todo) if there is an error, it will return the error
+		return err
+	}
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Todo body cannot be empty"})
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), todo) //after above if-checks have been passed, insert the todo into the database (collection)
+	if err != nil {
+		return err
+	}
+
+	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
+
+	return c.Status(201).JSON(todo) //status 201 shows that a resource has been created
+}
+
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id") //takes the id from the URL route parameter (The API Endpoint) *This will be a string
+
+	objectID, err := primitive.ObjectIDFromHex(id) //converts the id from a string to an opbjectID (What MongoDB uses for _id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+	}
+
+	filter := bson.M{"_id": objectID}                   //this filter specifies which document is needed to be updated
+	update := bson.M{"$set": bson.M{"completed": true}} //updates only the completed field to true
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
+}
+
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
+
+}
